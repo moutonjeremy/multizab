3b28abde67edf04d978c526031bfbcbc8a521bbe
jeremmm <moutonjeremy@labbs.fr>
2016-02-10 23:48:26 +0100

    change style trigger, add screen

--- multizab/api/controllers.py
+++ multizab/api/controllers.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from pyzabbix import ZabbixAPI
+from zabbix_client import ZabbixServerProxy
 
 from multizab.data.models import db, Zabbix
 
@@ -16,16 +17,16 @@ def hosts():
 def alerts():
     alerts_data = []
     for i in Zabbix.query.all():
-        zapi = ZabbixAPI('http://{0}'.format(i.host))
+        zapi = ZabbixAPI('http://{0}/zabbix/'.format(i.host))
         zapi.login(i.username, i.password)
         triggers = zapi.trigger.get(only_true=1,
-                                  skipDependent=1,
-                                  monitored=1,
-                                  active=1,
-                                  output='extend',
-                                  expandDescription=1,
-                                  expandData='host',
-                                  withLastEventUnacknowledged=1)
+                                    skipDependent=1,
+                                    monitored=1,
+                                    active=1,
+                                    output='extend',
+                                    expandDescription=1,
+                                    expandData='host',
+                                    withLastEventUnacknowledged=1)
         for j in triggers:
             j['platform'] = i.host
             alerts_data.append(j)

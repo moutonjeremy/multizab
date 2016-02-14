e132044277cde48e0872f50adb185b216eb1f5e8
jeremmm <moutonjeremy@labbs.fr>
2016-02-14 16:05:14 +0100

    remove redondency code

--- multizab/static/js/app.js
+++ multizab/static/js/app.js
@@ -5,73 +5,49 @@ app.config(['$interpolateProvider', function($interpolateProvider) {
   $interpolateProvider.endSymbol(']}');
 }]);
 
-app.controller('AlertsListCtrl', function ($scope, $http, $q, $interval){
+app.factory('GetFactory', function($http, $q) {
+    var factory = {
+        result : false,
+        getResult : function(path) {
+            var deferred = $q.defer();
+            $http.get(path)
+                .success(function (data, status) {
+                    factory.result = data;
+                    deferred.resolve(factory.result);
+                }).error(function (data, status) {
+                    deferred.reject('Error');
+                });
+                return deferred.promise;
+        }
+    }
+    return factory;
+});
+
+app.controller('AlertsListCtrl', function ($scope, $interval, GetFactory){
     this.loadNotifications = function (){
-        var deferred = $q.defer()
-        $http.get('api/alerts')
-            .success(function (data, status) {
-                deferred.resolve(data);
-            }).error(function (data, status){
-                deferred.reject('Error');
-            });
-            deferred.promise.then(function(results){
-                $scope.results = results;
-            })
+        GetFactory.getResult('api/alerts').then(function(result){
+            $scope.results = result
+        })
     }
 
     $interval(function(){
-       this.loadNotifications();
-    }.bind(this), 10000);
-
-    this.loadNotifications();
-
-})
-
-app.controller('CountPerZabbixCtrl', function ($scope, $http, $q, $interval){
-    this.loadNotifications = function (){
-        var deferred = $q.defer()
-        $http.get('api/count/alerts')
-            .success(function (data, status) {
-                deferred.resolve(data);
-            }).error(function (data, status){
-                deferred.reject('Error');
-            });
-            deferred.promise.then(function(results){
-                $scope.results = results.result;
-                $scope.labels = []
-                $scope.data = []
-                angular.forEach($scope.results, function(value, key){
-                    $scope.labels.push(key)
-                    $scope.data.push(value)
-                })
-            })
-    }
-
-    $interval(function(){
-       this.loadNotifications();
-    }.bind(this), 10000);
+        this.loadNotifications();
+    }.bind(this), 5000)
 
     this.loadNotifications();
 })
 
-app.controller('CountPerTypeCtrl', function ($scope, $http, $q, $interval){
+app.controller('CountPerZabbixCtrl', function ($scope, $interval, GetFactory){
     this.loadNotifications = function (){
-        var deferred = $q.defer()
-        $http.get('api/count/types')
-            .success(function (data, status) {
-                deferred.resolve(data);
-            }).error(function (data, status){
-                deferred.reject('Error');
-            });
-            deferred.promise.then(function(results){
-                $scope.results = results.result;
-                $scope.labels = []
-                $scope.data = []
-                angular.forEach($scope.results, function(value, key){
-                    $scope.labels.push(key)
-                    $scope.data.push(value)
-                })
+        GetFactory.getResult('api/count/alerts').then(function(result){
+            $scope.results = result.result;
+            $scope.labels = []
+            $scope.data = []
+            angular.forEach($scope.results, function(value, key){
+                $scope.labels.push(key)
+                $scope.data.push(value)
             })
+        })
     }
 
     $interval(function(){
@@ -81,18 +57,31 @@ app.controller('CountPerTypeCtrl', function ($scope, $http, $q, $interval){
     this.loadNotifications();
 })
 
-app.controller('ZabbixListCtrl', function ($scope, $http, $q, $interval){
+app.controller('CountPerTypeCtrl', function ($scope, $interval, GetFactory){
     this.loadNotifications = function (){
-        var deferred = $q.defer()
-        $http.get('api/list/zabbix')
-            .success(function (data, status) {
-                deferred.resolve(data);
-            }).error(function (data, status){
-                deferred.reject('Error');
-            });
-            deferred.promise.then(function(results){
-                $scope.results = results;
+        GetFactory.getResult('api/count/types').then(function(result){
+            $scope.results = result.result;
+            $scope.labels = []
+            $scope.data = []
+            angular.forEach($scope.results, function(value, key){
+                $scope.labels.push(key)
+                $scope.data.push(value)
             })
+        })
+    }
+
+    $interval(function(){
+       this.loadNotifications();
+    }.bind(this), 10000);
+
+    this.loadNotifications();
+})
+
+app.controller('ZabbixListCtrl', function ($scope, $interval, GetFactory){
+    this.loadNotifications = function (){
+        GetFactory.getResult('api/list/zabbix').then(function(result){
+            $scope.results = result
+        })
     }
 
     $interval(function(){
@@ -103,27 +92,21 @@ app.controller('ZabbixListCtrl', function ($scope, $http, $q, $interval){
 
 })
 
-app.controller('CountPerTypesZabbixCtrl', function ($scope, $http, $q, $interval){
+app.controller('CountPerTypesZabbixCtrl', function ($scope, $interval, GetFactory){
     $scope.init = function(id) {
         $scope.id = id;
     }
+
     this.loadNotifications = function (){
-        var deferred = $q.defer()
-        $http.get('api/count/types/'+$scope.id)
-            .success(function (data, status) {
-                deferred.resolve(data);
-            }).error(function (data, status){
-                deferred.reject('Error');
-            });
-            deferred.promise.then(function(results){
-                $scope.results = results.result;
-                $scope.labels = []
-                $scope.data = []
-                angular.forEach($scope.results, function(value, key){
-                    $scope.labels.push(key)
-                    $scope.data.push(value)
-                })
+        GetFactory.getResult('api/count/types/'+$scope.id).then(function(result){
+            $scope.results = result.result;
+            $scope.labels = []
+            $scope.data = []
+            angular.forEach($scope.results, function(value, key){
+                $scope.labels.push(key)
+                $scope.data.push(value)
             })
+        })
     }
 
     $interval(function(){

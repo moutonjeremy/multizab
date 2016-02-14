1e7204f5c5d73addabaaadc11fc8a3d2f485983f
jeremmm <moutonjeremy@labbs.fr>
2016-02-14 23:31:54 +0100

    fix missing controller

--- multizab/static/js/app.js
+++ multizab/static/js/app.js
@@ -9,19 +9,44 @@ app.factory('CacheFactory', function($cacheFactory){
     return $cacheFactory('alerts_cache')
 })
 
-app.controller('RefreshCacheCtrl', function($scope, $http, $q, $interval, CacheFactory){
-    var deferred = $q.defer();
-    $http.get('api/graphics')
-        .success(function (data, status) {
-            deferred.resolve(data);
-        }).error(function (data, status) {
-            deferred.reject('Error');
-        });
-    deferred.promise.then(function(result){
-        angular.forEach(result.result, function(value, key){
+app.factory('GetFactory', function($http, $q){
+    var factory = {
+        result : false,
+        getResult : function(path) {
+            var deferred = $q.defer();
+            $http.get(path)
+                .success(function (data, status) {
+                    factory.result = data;
+                    deferred.resolve(factory.result);
+                }).error(function (data, status) {
+                    deferred.reject('Error');
+                });
+                return deferred.promise;
+        }
+    }
+    return factory;
+})
+
+app.controller('RefreshCacheCtrl', function($scope, $interval, GetFactory, CacheFactory){
+    GetFactory.getResult('api/graphics').then(function(result){
+            angular.forEach(result.result, function(value, key){
             CacheFactory.put(key, value)
-            })
-    });
+        })
+    })
+})
+
+app.controller('AlertsListCtrl', function ($scope, $interval, GetFactory){
+    this.loadNotifications = function (){
+        GetFactory.getResult('api/alerts').then(function(result){
+            $scope.results = result
+        })
+    }
+
+    $interval(function(){
+        this.loadNotifications();
+    }.bind(this), 5000)
+
+    this.loadNotifications();
 })
 
 app.controller('CountAlertsCtrl', function($scope, $interval, CacheFactory){
